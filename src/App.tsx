6a335db32528a0ac745bbef907dfe12ceaf0dbdf
Sebastian Gonzalez C <66439344+SGCifuentes@users.noreply.github.com>
2022-01-08 12:52:07 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
+      <h1 className="text-3xl font-bold underline">
+      Hello world
+      </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
